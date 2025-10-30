5bf6bc3ad140ec5be57014db52f58ef9f5148a22
aryawidiap <79260381+aryawidiap@users.noreply.github.com>
2025-10-30 22:51:19 +0800

    add all components when the page is fullest

--- app/page.tsx
+++ app/page.tsx
@@ -4,60 +4,76 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        <h1>Sambung Lagu</h1>
+        <div id="history" className="flex flex-col-reverse">
+          <div id="album21092" className="flex flex-row p-3 outline rounded-xl bg-white/30">
+            <div className="album-cover rounded-md bg-blue-100 size-[72px]"></div>
+            <div className="song-description ml-3">
+              <div className="flex flex-row ">
+                <h3>Time for the moon night</h3>
+                <div className="ml-2">2018</div>
+              </div>
+              <div>
+                GFRIEND
+              </div>
+            </div>
+          </div>
+          <div id="album21094" className="flex flex-row p-3 outline rounded-xl bg-white/30 blur-[2px] scale-90">
+            <div className="album-cover rounded-md bg-blue-100 size-[72px]"></div>
+            <div className="song-description ml-3">
+              <div className="flex flex-row ">
+                <h3>Time for the moon night</h3>
+                <div className="ml-2">2018</div>
+              </div>
+              <div>
+                GFRIEND
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <form action="post">
+          <div className="sm:col-span-4">
+            <label htmlFor="judulLagu" className="block text-center">Judul lagu</label>
+            <input name="judulLagu" type="text" className="block mt-3 min-w-0 grow bg-stone-900 outline rounded-full focus:outline-blue-300 sm:text-sm/6 py-1.5 px-3 text-center" placeholder="Ketik judul untuk mulai" />
+          </div>
+          <div className="sm:col-span-4 mt-6">
+            <label htmlFor="penyanyi" className="block text-center">Penyanyi</label>
+            <input name="penyayi" type="text" className="block mt-3 min-w-0 grow bg-stone-900 outline rounded-full focus:outline-blue-300 sm:text-sm/6 py-1.5 px-3 text-center" placeholder="Ketik nama penyanyi" />
+          </div>
+          <div className="mt-6 flex items-center justify-center gap-x-6">
+            <button type="submit" className="rounded-full bg-indigo-500 px-6 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Cari</button>
+          </div>
+        </form>
+        <div>
+          <div>
+            Manakah lagu yang kamu maksud?
+          </div>
+          <div id="history" className="flex flex-col gap-y-3">
+            <div id="album21092" className="flex flex-row p-3 outline rounded-xl bg-white/30">
+              <div className="album-cover rounded-md bg-blue-100 size-[72px]"></div>
+              <div className="song-description ml-3">
+                <div className="flex flex-row ">
+                  <h3>Time for the moon night</h3>
+                  <div className="ml-2">2018</div>
+                </div>
+                <div>
+                  GFRIEND
+                </div>
+              </div>
+            </div>
+            <div id="album21092" className="flex flex-row p-3 outline rounded-xl bg-white/30">
+              <div className="album-cover rounded-md bg-blue-100 size-[72px]"></div>
+              <div className="song-description ml-3">
+                <div className="flex flex-row ">
+                  <h3>Time for the moon night</h3>
+                  <div className="ml-2">2018</div>
+                </div>
+                <div>
+                  GFRIEND
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </div>
