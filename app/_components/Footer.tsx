01e0c0cdfaa400928225f4c6ac03fcf88bcc1d8d
aryawidiap <79260381+aryawidiap@users.noreply.github.com>
2026-02-15 21:38:40 +0800

    fix redesign footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,9 +1,15 @@
 'use client';
 
+import Link from "next/link";
+
 export default function Footer() {
-    return <footer className="flex flex-row gap-2 my-3">
-        <div>Made with MusicBrainz DB |</div>
-        <div>Instagram |</div>
-        <div>Github</div>
+    return <footer className="flex flex-col gap-2 md:flex-row md:gap-5 my-3">
+        <Link className="hover:underline hover:text-transparent hover:bg-linear-to-r hover:from-fuchsia-600 hover:to-orange-600 hover:bg-clip-text hover:text-shadow-md hover:text-shadow-white/10" href="https://musicbrainz.org/doc/MusicBrainz_API">
+            Dibuat dengan MusicBrainz DB
+        </Link>
+        <div className="flex flex-row gap-5 mx-auto">
+            <Link className="hover:underline hover:text-transparent hover:bg-linear-to-r hover:from-white hover:via-white hover:to-gray-600 hover:bg-clip-text hover:text-shadow-md hover:text-shadow-white/10" href="https://github.com/aryawidiap">Github</Link>
+            <Link className="hover:underline hover:text-transparent hover:bg-linear-to-r hover:from-fuchsia-500 hover:via-orange-500 hover:to-amber-300 hover:bg-clip-text hover:text-shadow-md hover:text-shadow-white/10" href="https://instagram.com/aryaw.portfolio">Instagram</Link>
+        </div>
     </footer>;
 }
