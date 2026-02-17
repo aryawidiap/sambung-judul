d1e82e516e1456ddc18aa40cae156cf141a5942a
aryawidiap <79260381+aryawidiap@users.noreply.github.com>
2026-02-17 10:02:22 +0800

    Create content.ts

---
+++ app/_utils/content.ts
@@ -0,0 +1,39 @@
+/**
+* Contents for localization
+*/
+
+const main = {
+about: {
+header: {
+id: '',
+en: '',
+},
+content: {
+id: '',
+en: '',
+},
+},
+howto: {
+header: {
+id: '',
+en: '',
+},
+content: {
+id: '',
+en: '',
+},
+},
+newfile: {},
+}
+
+const searchForm = {
+title: {},
+artist: {},
+}
+
+const searchResult = {
+header: {},
+notFoundMessage: {},
+}
+
+const searchHistory = {}
