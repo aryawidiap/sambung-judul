77218b421170d1938c43ef5109920559fbeefa1c
aryawidiap <79260381+aryawidiap@users.noreply.github.com>
2026-02-24 21:50:30 +0800

    fix: change song title tooltip into wrap animation instead

--- app/_components/SongDetails.tsx
+++ app/_components/SongDetails.tsx
@@ -1,7 +1,13 @@
+import { AnimatePresence, motion } from "motion/react";
 import Song from "../_model/Song";
 import SongImage from "./SongImage";
 
-export default function SongDetails({ song }: { song: Song; }) {
+export default function SongDetails({ song, buttonFocus }: { song: Song, buttonFocus: boolean }) {
+
+    if (buttonFocus) {
+        console.log('Button is focus');
+
+    }
     return (
         <div className="flex flex-row items-center p-3 me-1">
             <div className="album-cover rounded-md bg-blue-100 size-[80px] min-w-[80px] min-h-[80px] overflow-clip">
@@ -9,8 +15,19 @@ export default function SongDetails({ song }: { song: Song; }) {
             </div>
             <div className="ml-3 min-w-0 text-stone-700">
                 <div className="group relative w-100">
-                    <h3 className="text-md font-semibold w-2xs overflow-hidden text-nowrap text-ellipsis capitalize w-100">{song.title}</h3>
-                    <div className="tooltip absolute invisible group-hover:visible bg-neutral-200 text-stone-900 rounded transition px-2 py-1 text-sm text-center bottom-[100%] capitalize">{song.title}</div>
+                    <AnimatePresence>
+                        <motion.h3
+                            style={{
+                                height: buttonFocus ? "auto" : "1lh",
+                                textWrap: buttonFocus ? "wrap" : "nowrap"
+                            }}
+                            transition={{ ease: "easeInOut" }}
+                            whileHover={{ height: "auto", textWrap: "wrap" }}
+                            className="text-md font-semibold w-2xs overflow-hidden text-ellipsis capitalize w-50"
+                        >
+                            {song.title}
+                        </motion.h3>
+                    </AnimatePresence>
                 </div>
                 <div className="text-sm">{song.artist}</div>
                 <div className="text-sm w-[4ch]">{song.year}</div>
--- app/_components/SongSearchListItem.tsx
+++ app/_components/SongSearchListItem.tsx
@@ -5,12 +5,23 @@ import { IconProp, library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { motion } from "motion/react";
 import SongDetails from "./SongDetails";
+import { useState } from "react";
 
 
 export default function SongSearchListItem({ song, addSong }: SongSearchListItemProps) {
     library.add(fas);
     // @ts-expect-error Type '"fa-solid fa-angle-right"' is not assignable to type 'IconProp'.
     const angleRightIcon: IconProp = "fa-solid fa-angle-right";
+    
+    const [addButtonIsFocused, setAddButtonIsFocused] = useState(false);
+
+    const handleAddButtonFocus = () => {
+        setAddButtonIsFocused(true);
+    }
+
+    const handleAddButtonBlur = () => {
+        setAddButtonIsFocused(false);
+    }
     
     return (
         <motion.li
@@ -19,7 +30,7 @@ export default function SongSearchListItem({ song, addSong }: SongSearchListItem
             exit={{ opacity: 0 }}
             id={song.id}
             className="outline rounded-xl bg-white/50 backdrop-blur-xs relative w-xs ">
-            <SongDetails song={song} />
+            <SongDetails song={song} buttonFocus={addButtonIsFocused}/>
             <button className="ps-5 absolute inset-y-0 right-0 
       text-white/50
       hover:bg-linear-to-r hover:from-white/0 hover:to-white/80 
@@ -28,7 +39,11 @@ export default function SongSearchListItem({ song, addSong }: SongSearchListItem
       rounded-r-xl
       transition" onClick={() => {
                     addSong(song)
-                }}>
+                }}
+                
+                onFocus={handleAddButtonFocus}
+                onBlur={handleAddButtonBlur}
+                >
                 <FontAwesomeIcon icon={angleRightIcon} />
             </button>
         </motion.li>
