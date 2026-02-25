5d6918f9ad062a7721d9ee91d5cc5b0f758a5341
aryawidiap <79260381+aryawidiap@users.noreply.github.com>
2026-02-25 23:02:08 +0800

    add: Navigation component is now separated from the page, to make the file less crowded

---
+++ app/_components/Navigation.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { FaInfo, FaQuestion, FaPlus } from 'react-icons/fa6';
+import { figtree } from '../fonts';
+import { msMadi } from '../page';
+
+export function Navigation({ initialPage, newGame, openAboutModal, openHowToModal, startNewGame, changeLanguage, language }: { initialPage: boolean; newGame: boolean; openAboutModal: () => void; openHowToModal: () => void; startNewGame: () => void; changeLanguage: (language: string) => void; language: string }) {
+    const newGameButtonClassName = newGame ?
+        "position-absolute cursor-not-allowed opacity-50"
+        : "position-absolute cursor-pointer hover:*:fill-blue-200";
+
+    return <nav>
+        <h1 className={"text-3xl my-3 transition " + msMadi.className + (initialPage ? "" : "")}>Sambung
+            <span className={figtree.className + " font-bold uppercase text-2xl"}>Judul</span>
+        </h1>
+        <div className='flex flex-row gap-3 justify-center'>
+            <button className="position-absolute cursor-pointer hover:*:fill-blue-200" onClick={openAboutModal}><FaInfo /></button>
+            <button className="position-absolute cursor-pointer hover:*:fill-blue-200" onClick={openHowToModal}><FaQuestion /></button>
+            <button className={newGameButtonClassName} onClick={startNewGame} disabled={newGame}><FaPlus /></button>
+            <button className="position-absolute cursor-pointer hover:*:fill-blue-200" onClick={() => {language === 'id' ? changeLanguage('en') : changeLanguage('id')}}>{language}</button>
+        </div>
+    </nav>;
+}
