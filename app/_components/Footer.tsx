94adc40a0ac73c71357ce14298deeecf563bd5c6
aryawidiap <79260381+aryawidiap@users.noreply.github.com>
2026-02-25 23:02:52 +0800

    fix: Footer content localization

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import Link from "next/link";
+import { footer as footerContent } from "../_utils/content";
 
 export default function Footer() {
     return <footer className="flex flex-col gap-2 md:flex-row md:gap-5 my-3">
         <Link className="hover:underline hover:text-transparent hover:bg-linear-to-r hover:from-fuchsia-600 hover:to-orange-600 hover:bg-clip-text hover:text-shadow-md hover:text-shadow-white/10" href="https://musicbrainz.org/doc/MusicBrainz_API">
-            Dibuat dengan MusicBrainz DB
+            {footerContent.madeWith['en']}
         </Link>
         <div className="flex flex-row gap-5 mx-auto">
             <Link className="hover:underline hover:text-transparent hover:bg-linear-to-r hover:from-white hover:via-white hover:to-gray-600 hover:bg-clip-text hover:text-shadow-md hover:text-shadow-white/10" href="https://github.com/aryawidiap">Github</Link>
