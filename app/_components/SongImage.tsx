22ae6dc092026e49d3cb06ced7b6a84695caf243
aryawidiap <79260381+aryawidiap@users.noreply.github.com>
2026-02-27 16:38:55 +0800

    fix: Reconfigure src for Images

--- app/_components/SongImage.tsx
+++ app/_components/SongImage.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function SongImage({ songCoverArtLink }: { songCoverArtLink: string; }) {
     const getActualSrc = (coverLink: string) => {
         if (coverLink === '') {
-            return `/music_placeholder.png`;
+            return `./music_placeholder.png`;
         }
         return coverLink;
     }
-    return <Image src={getActualSrc(songCoverArtLink)} alt="" className="object-cover h-auto w-full" height={300} width={300} placeholder="blur" blurDataURL="/music_placeholder.png"/>;
+    return <Image src={getActualSrc(songCoverArtLink)} alt="" className="object-cover h-auto w-full" height={300} width={300} placeholder="blur" blurDataURL="./music_placeholder.png"/>;
 }
